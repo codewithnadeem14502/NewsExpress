0f4791766adef6501cee6f0ececa43c609993eac
MohdNadeem <codewithnadeem@gmail.com>
2023-03-07 03:35:50 +0530

    done

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+
+Setup:
+- run ```npm i && npm start``` to start development server
---
+++ build/precache-manifest.9ccd118facedb3d39f99711501fac063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7609aa9e0bfd316010c7f7b02d963ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9acdbc09a7df95cd356",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "78f7bacd4ad77c7cc761",
+    "url": "/static/js/2.dd8efa81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dd8efa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9acdbc09a7df95cd356",
+    "url": "/static/js/main.4c76fdb4.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "f262d7e5366238c5d9721c5eeead0eb7",
+    "url": "/static/media/logo.f262d7e5.png"
+  }
+]);
